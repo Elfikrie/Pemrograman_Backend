164de9c84830014837e2f05fff26fe0c84bdbc85
Elfikrie <m.fikrie48@gmail.com>
2025-01-07 10:02:46 +0700

    upload file

--- routes/api.js
+++ routes/api.js
@@ -5,33 +5,10 @@ const express = require("express");
 const router = express.Router();
 
 // Routing
-// Menampilkan data students
-// router.get("/students", (req, res) => {
-//   res.send("Menampilkan semua data students");
-// });
 router.get("/students", StudentController.index);
-
-// Menambahkan data students
-// router.post("/students", (req, res) => {
-//   res.send("Menambahkan data students");
-// });
+router.get("/students/:id", StudentController.show);
 router.post("/students", StudentController.store);
-// MEngupdate data students
-// router.put("/students/:id", (req, res) => {
-//   const { id } = req.params;
-//   res
-//     .status(201)
-//     .json({
-//       message : `Mengedit data students ${id}`,
-//       data: req.body
-//     })
-// });
 router.put("/students/:id", StudentController.update);
-// Menghapus data students
-// router.delete("/students/:id", (req, res) => {
-//   const { id } = req.params;
-//   res.send(`Menghapus data student ${id}`);
-// });
 router.delete("/students/:id", StudentController.destroy);
 
 module.exports = router;
